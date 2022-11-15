ce64944a0532b8010b75164648002e6d7686efb9
kavindukaweeshvara <101484992+kavindukaweeshvara@users.noreply.github.com>
2022-11-15 20:19:58 +0530

    settings.js

--- settings.js
+++ settings.js
@@ -42,7 +42,7 @@ global.socialm = "GitHub: prabhasha2006" // github or insta name
 global.location = "India, Mizoram, Aizawl" // location
 
 //bot body 
-global.alivepic = `https://i.ibb.co/cyyCjN7/20220803-081426.png` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
+global.alivepic = `https://i.ibb.co/X2BrTrm/IMG-20221115-WA0013.jpg` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
 
 global.dragonchat = "false"  // chat bot  "true" / "false" (200 auto replies in this)
 global.callblock = "false" // call block "true" / "false" ("true"  to block callers)
